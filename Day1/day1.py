6270f0f47696c750ed10c44b2f1913d12be1fee4
wakabibrian <wakabibrian95@gmail.com>
2023-03-31 20:08:53 +0300

    Finished the printing exercise

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,2 +1,13 @@
 #-----------------Printing--------------------#
-print("Hello World!") #Use the print() function
+# print("Hello World!") #Use the print() function
+
+#Printing Exercise
+    #Write a Python code which prints the lines as below
+    ###########
+    #Day 1 - Python Print Function
+    #The function is declared like this:
+    #print('what to print')
+    ############
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
