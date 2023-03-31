dab6a45abb0542daaab88f9ca0bb35d6ac3e2d59
wakabibrian <wakabibrian95@gmail.com>
2023-03-31 20:31:29 +0300

    Finished the string manipulation example

--- Day1/Day1.md
+++ Day1/Day1.md
@@ -2,5 +2,7 @@
 I am learning "Working With Variables" today.
 
 Today I learn't;
-- Printing
-- 
+- Printing - print() function
+- String Manipulation:
+    - Separating string lines using \n
+    - String concatenation with +
--- Day1/day1.py
+++ Day1/day1.py
@@ -1,4 +1,4 @@
-#-----------------Printing--------------------#
+#-----------------Printing - print() function--------------------#
 # print("Hello World!") #Use the print() function
 
 #Printing Exercise
@@ -8,6 +8,12 @@
     #The function is declared like this:
     #print('what to print')
     ############
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+#-----------------Separating string lines using \n--------------------#
+# print("This is the first line.\nThis is the second line.")
+
+#-----------------String concatenation with +--------------------#
+print("Hello" + " Wakabi")
