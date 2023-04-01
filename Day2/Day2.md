7a31b845b7b051ee18461b5b5003c0094ae73328
wakabibrian <wakabibrian95@gmail.com>
2023-04-01 09:54:05 +0300

    Finished type conversion examples

--- Day2/Day2.md
+++ Day2/Day2.md
@@ -3,3 +3,4 @@ I am learning "Understanding Data Types" today.
 
 Today I learn't;
 1. Primitive Data Types
+2. Type Errors, Type Checking and Type Conversations
--- Day2/day2.py
+++ Day2/day2.py
@@ -6,11 +6,25 @@
 # print("123" + "456")
 
 # Interger - Numbers without a decimal point - Whole numbers
-print(123 + 456)
-print(123_456_789) # For visualisation
+# print(123 + 456)
+# print(123_456_789) # For visualisation
 
 # Float - Numbers with decimal places (Floating point number)
-print(45.67)
+# print(45.67)
 
 # Boolean
 # It has two possible values; True or False
+
+# -----------------Type Errors, Type Checking and Type Conversations--------------------#
+# len(234) # Gives a TypeError
+
+# num_char = len(input("What is your name? "))
+# print("Your name has " + num_char + " characters") # Gives an error
+# print(type(num_char)) # Type Checking
+
+# Type convertion or type casting: changing a piece of data from one data type to another.
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters")
+
+# print(70 + float("170.5"))
+
