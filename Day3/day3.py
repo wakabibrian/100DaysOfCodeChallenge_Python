26bdc52d8eac308d581afc6cc5fe97024298f458
wakabibrian <wakabibrian95@gmail.com>
2023-04-01 21:29:36 +0300

    Finished BMI exercise

--- Day3/day3.py
+++ Day3/day3.py
@@ -50,5 +50,20 @@
 # Above 35 they are clinically obese
 
 # Solution
+height = float(input("Enter height in m: "))
+weight = int(input("Enter weight in kg: "))
+bmi_float = weight / height ** 2
+bmi = round(bmi_float, 2)
+
+if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
 
 
