e924612d2b564385bcfc95730000dae0052310e4
wakabibrian <wakabibrian95@gmail.com>
2023-04-01 14:01:52 +0300

    Finished Life in Weeks Exercise

--- Day2/day2.py
+++ Day2/day2.py
@@ -65,9 +65,24 @@
 # print(8//3) # Chops digits after a decimal point (Whole number)
 
 #Number manipulation
-score = 0
-score += 1 # Same as score = score + 1; Also use s
+# score = 0
+# score += 1 # Same as score = score + 1; Also use s
 # print(score)
 
 # F strings
-print(f"Your score is {score}")
+# print(f"Your score is {score}")
+
+# -----------------Your life in Weeks Exercise--------------------#
+# Create a program using maths and f-Strings that tells us how many days, 
+    # weeks, months we have left if we live until 90 years old.
+# It will take your current age as the input and output a message with our time left in this format:
+# You have x days, y weeks, and z months left.
+# Where x, y and z are replaced with the actual calculated numbers.
+
+age = int(input("What is your current age? "))
+years_left = 90 - age
+months_left = years_left*12
+weeks_left = years_left*52
+days_left = years_left * 365
+
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left")
