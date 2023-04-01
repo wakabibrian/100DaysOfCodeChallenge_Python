ba251748eb243bf4019d495cdaa532c09eef61fb
wakabibrian <wakabibrian95@gmail.com>
2023-04-01 09:29:42 +0300

    Finished primitive data types

--- Day2/Day2.md
+++ Day2/Day2.md
@@ -2,3 +2,4 @@
 I am learning "Understanding Data Types" today.
 
 Today I learn't;
+1. Primitive Data Types
--- Day2/day2.py
+++ Day2/day2.py
@@ -1,1 +1,16 @@
-# -----------------Printing - print() function--------------------#
+# -----------------Primitive Data Types--------------------#
+# Strings - string of characters ("")
+# print("Hello"[0]) #Subscripting
+# print("Hello"[4])
+# print("Hello"[-1])
+# print("123" + "456")
+
+# Interger - Numbers without a decimal point - Whole numbers
+print(123 + 456)
+print(123_456_789) # For visualisation
+
+# Float - Numbers with decimal places (Floating point number)
+print(45.67)
+
+# Boolean
+# It has two possible values; True or False
