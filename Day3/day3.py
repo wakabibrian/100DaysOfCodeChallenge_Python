2d22f6a25e247adf9de36ec79f0fe87eea94c814
wakabibrian <wakabibrian95@gmail.com>
2023-04-02 23:44:03 +0300

    finished the love calculator exercise

--- Day3/day3.py
+++ Day3/day3.py
@@ -160,11 +160,47 @@
 # or operator, A or B: one of A or B have to be True
 # not operator, not A: reverses a condition
 
+### Love Calculator Exercise
+# You are going to write a program that tests the compatibility between two people.
+# To work out the love score between two people:
+#   Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+#   Then check for the number of times the letters in the word LOVE occurs. 
+#   Then combine these numbers to make a 2 digit number.
 
+# For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is **x**, you go together like coke and mentos."
 
+# For Love Scores between 40 and 50, the message should be:
+# "Your score is **y**, you are alright together."
 
+# Otherwise, the message will just be their score. e.g.:
+# "Your score is **z**."
 
+# Solution
+print("Welcome to the love caculator!")
+name1 = input("What is your name?\n").lower()
+name2 = input("What is their name?\n").lower()
+name = name1 + name2
+num1, num2 = 0, 0
 
+num1 += name.count("t")
+num1 += name.count("r")
+num1 += name.count("u")
+num1 += name.count("e")
 
+num2 += name.count("l")
+num2 += name.count("o")
+num2 += name.count("v")
+num2 += name.count("e")
+
+score_str = str(num1) + str(num2)
+score = int(score_str)
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
 
 
