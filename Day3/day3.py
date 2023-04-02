9c1ba8314c8cd59d4edecf51c6b0501ce7465cc8
wakabibrian <wakabibrian95@gmail.com>
2023-04-02 10:40:29 +0300

    Finished if statements

--- Day3/day3.py
+++ Day3/day3.py
@@ -92,3 +92,28 @@
 # -----------------Multiple if statement in succession--------------------#
 # Checking multiple conditions even if the first one was true
 
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill =0
+
+if height >120:
+    print("You can ride the rollcoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        bill = 5
+        print(f"Child tickets are for ${bill}.")
+    elif age <= 18:
+        bill = 7
+        print(f"Youth tickets are for ${bill}.")
+    else:
+        bill = 12
+        print(f"Adult tickets are for ${bill}.")
+
+    wants_photo = input("Do you want photo? Y or N: ")
+    if wants_photo == "Y":
+        bill += 3
+
+    print(f"Your total bill is ${bill}")
+else:
+    print("Sorry you have to grow before you can ride.")
+
