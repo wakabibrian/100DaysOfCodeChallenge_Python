bf7651e99dfd000cedd35b5592e1ef13340d3ceb
wakabibrian <wakabibrian95@gmail.com>
2023-04-03 22:02:20 +0300

    Finished random Module

---
+++ Day4/day4.py
@@ -0,0 +1,18 @@
+# -----------------Random Module-------------------#
+import random
+import my_module
+
+# Module helps in splitting the code.
+# Radom module holds functions for random numbers 
+
+random_integer = random.randint(1,10)
+# print(random_integer)
+
+random_float = random.random()
+print(random_float*5)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
+
+# print(my_module.pi)
+
