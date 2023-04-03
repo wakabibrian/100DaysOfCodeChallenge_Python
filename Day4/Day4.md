4efb78ceae621ef16267c70b116d61f619113ff0
wakabibrian <wakabibrian95@gmail.com>
2023-04-03 22:42:05 +0300

    Finished lists examples

--- Day4/Day4.md
+++ Day4/Day4.md
@@ -4,3 +4,4 @@ I am learning "Randomisation and Python Lists" today.
 Today I learn't;
 1. Random Module
 2. Creating a Module
+3. Lists
--- Day4/day4.py
+++ Day4/day4.py
@@ -1,9 +1,9 @@
 # -----------------Random Module-------------------#
-import random
+# import random
 # import my_module
 
 # Module helps in splitting the code.
-# Radom module holds functions for random numbers 
+# Radom module holds functions for random numbers
 
 # random_integer = random.randint(1,10)
 # print(random_integer)
@@ -18,9 +18,26 @@ import random
 
 # Exercise
 # You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
-random_number = random.randint(0,1)
+# random_number = random.randint(0,1)
 
-if random_number == 0:
-    print("Heads")
-else:
-    print("Tails")
+# if random_number == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+# -----------------Lists-------------------#
+# A list is Data structure, its just a way of storing and organising data in Python,
+# grouped pieces of data with a connection - Orderly; Order matters
+
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine",
+#                      "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# print(states_of_america[0])
+# print(states_of_america[-1]) # Starts counting from the end of the list
+
+# states_of_america[1] = "Pencilvania"
+# states_of_america.append("Agelland") # Appending an item to the end of the list
+# states_of_america.extend(["Wakaland", "Kayland", "Loylan"]) # Adding a list to a list
+print(states_of_america)
+
+# Exercise
