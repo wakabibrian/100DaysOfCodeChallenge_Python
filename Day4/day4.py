8bf4848f8e022d45bb9e438d91b0cc5e9ea389da
wakabibrian <wakabibrian95@gmail.com>
2023-04-05 00:22:08 +0300

    Completed Day 4

--- Day4/day4.py
+++ Day4/day4.py
@@ -116,19 +116,20 @@ user_selection = int(
     input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
 computer_selection = random.randint(0, 2)
 
-print(choices[user_selection])
-print("Computer chose:\n")
-print(choices[computer_selection])
-
 if user_selection >= 3 or user_selection < 0:
     print("You typed an invalid number, you loose!")
-elif computer_selection == user_selection:
-    print("It's a draw")
-elif user_selection == 0 and computer_selection == 2:
-    print("You Win!")
-elif computer_selection == 0 and user_selection == 2:
-    print("You Loose!")
-elif user_selection > computer_selection:
-    print("You Win!")
-elif user_selection < computer_selection:
-    print("You Loose!")
+else:
+    print(choices[user_selection])
+    print("Computer chose:\n")
+    print(choices[computer_selection])
+
+    if computer_selection == user_selection:
+        print("It's a draw")
+    elif user_selection == 0 and computer_selection == 2:
+        print("You Win!")
+    elif computer_selection == 0 and user_selection == 2:
+        print("You Loose!")
+    elif user_selection > computer_selection:
+        print("You Win!")
+    elif user_selection < computer_selection:
+        print("You Loose!")
