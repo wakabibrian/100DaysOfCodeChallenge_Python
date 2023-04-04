41b2562bd7078be4590c7ab1aca5dddfcfc8521d
wakabibrian <wakabibrian95@gmail.com>
2023-04-04 19:08:50 +0300

    started index Errors

--- Day4/Day4.md
+++ Day4/Day4.md
@@ -5,3 +5,4 @@ Today I learn't;
 1. Random Module
 2. Creating a Module
 3. Lists
+4. IndexErrors and Working with Nested Lists
--- Day4/day4.py
+++ Day4/day4.py
@@ -43,13 +43,15 @@
 # Exercise
 # You are going to write a program that will select a random name from a list of names.
 # The person selected will have to pay for everybody's food bill.
-import random
+# import random
 
-name_string = input("Give everybody's names sepparated by a comma: ")
-names = name_string.split(", ")
+# name_string = input("Give everybody's names sepparated by a comma: ")
+# names = name_string.split(", ")
 
-rand_name_number = random.randint(0, len(names)-1)
-print(rand_name_number)
+# rand_name_number = random.randint(0, len(names)-1)
+# print(rand_name_number)
 
-random_person = names[rand_name_number]
-print(f"{random_person} is going to buy the meal today!")
+# random_person = names[rand_name_number]
+# print(f"{random_person} is going to buy the meal today!")
+
+# -----------------IndexErrors and Working with Nested Lists-------------------#
