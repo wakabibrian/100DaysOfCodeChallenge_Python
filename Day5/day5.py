0adf0748f0b7241bf375428f0e2f1e87f743c2f3
wakabibrian <wakabibrian95@gmail.com>
2023-04-05 19:01:44 +0300

    Finished even numbers exercise

--- Day5/day5.py
+++ Day5/day5.py
@@ -66,8 +66,16 @@
 
 #### Exercise: 
 # Adding all the numbers between 1 and 100
-sum = 0
-for number in range (1, 101):
-    sum += number
+# sum = 0
+# for number in range (1, 101):
+#     sum += number
 
+# print(sum)
+
+#### Exercise:
+# You are going to write a program that calculates the sum of all the even numbers from 1 to 100. 
+# Thus, the first even number would be 2 and the last one is 100:
+sum = 0
+for number in range(2, 101, 2):
+    sum += number
 print(sum)
