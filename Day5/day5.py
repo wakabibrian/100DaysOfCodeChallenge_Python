1164d4c2970014ea3570402ff3e5b55fe31185db
wakabibrian <wakabibrian95@gmail.com>
2023-04-05 19:11:44 +0300

    Finished fizz buzz exercisee

--- Day5/day5.py
+++ Day5/day5.py
@@ -75,7 +75,24 @@
 #### Exercise:
 # You are going to write a program that calculates the sum of all the even numbers from 1 to 100. 
 # Thus, the first even number would be 2 and the last one is 100:
-sum = 0
-for number in range(2, 101, 2):
-    sum += number
-print(sum)
+# sum = 0
+# for number in range(2, 101, 2):
+#     sum += number
+# print(sum)
+
+####### Exercise 
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+#   Your program should print each number from 1 to 100 in turn.
+#   When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+#   When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
+#   And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
