a37928e83233c7bdc5a47cb910645a31a89214fb
wakabibrian <wakabibrian95@gmail.com>
2023-04-07 20:49:01 +0300

    Finished multiple arguments

--- Day8/day8.py
+++ Day8/day8.py
@@ -11,11 +11,23 @@
 # greet()
 
 # Variable names inside () gives funtions inputs
-def greet_with_name(name):
-    print(f"Hello {name}") 
-    print(f"How do you do {name}?") 
+# def greet_with_name(name):
+#     print(f"Hello {name}") 
+#     print(f"How do you do {name}?") 
 
-greet_with_name("Brian")
+# greet_with_name("Brian")
 
 # Parament - name of variable
 # Argument - the value passed to the variable when being called
+
+# -----------------Funtions with more than 1 input, Positional vs Keyword arguments-------------------#
+# def greet_with_name(name, location):
+#     print(f"Hello, my name is {name}")
+#     print(f"I am from {location}")
+
+# greet_with_name("Wakabi", "Maganjo")
+
+# The above are called positional arguments because they arguments are based on position of parameters
+# Keyword arguments we assign values to variable names when calling the function, we dont mind about the position.
+
+# greet_with_name(location="Kampala", name="Wakabi")
