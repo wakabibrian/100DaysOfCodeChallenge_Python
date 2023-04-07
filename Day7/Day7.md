7a9ecc8d7248eb8a949e15e3f05ed023d12c3fab
wakabibrian <wakabibrian95@gmail.com>
2023-04-07 13:28:09 +0300

    Completed Day 7

--- Day7/Day7.md
+++ Day7/Day7.md
@@ -7,3 +7,5 @@ Steps in the Project;
 3. Replacing Blanks with Guesses
 4. Checking if the player has won
 5. Keeping Track of player's lives
+6. Importing modules
+7. How to clear screen
--- Day7/day7.py
+++ Day7/day7.py
@@ -6,104 +6,55 @@
 # -----------------Challenge2: Replacing Blanks with Guesses-------------------#
 # -----------------Challenge3: Checking if the player has won-------------------#
 # -----------------Challenge4: Keeping Track of player's lives-------------------#
+# -----------------Challenge5: Importing modules and improving User Experience-------------------#
+
 import random
+import hangman_art
+import hangman_words
+import os
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
+end_game = False
+word_list = hangman_words.word_list
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 lives = 6
+clear = lambda: os.system("clear")
 
-# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
-# Set 'lives' to equal 6.
+logo = hangman_art.logo
+print(logo)
 
-# Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# # Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
 
 # Create blanks
 display = []
 for _ in range(word_length):
     display.append("_")
 
-end_game = False
-
 while not end_game:
     guess = input("Guess a letter: ").lower()
+    clear()
+
+    if guess in display:
+        print(f"You have already guessed {guess}")
 
     # Check guessed letter
     for position in range(word_length):
         if chosen_word[position] == guess:
             display[position] = guess
-    
-    print(f"{' '.join(display)}")
-   
 
-     # TODO-2: - If guess is not a letter in the chosen_word,
-    # Then reduce 'lives' by 1.
-    # If lives goes down to 0 then the game should stop and it should print "You lose."
+     
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that is not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_game = True
+            print("You Loose!")
+
+    print(f"{' '.join(display)}")
 
     if "_" not in display:
-        print("You win!")
         end_game = True
-    elif lives == 0:
-        end_game == True
-        print("You Loose!")
+        print("You win!")
 
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
-
+    print(hangman_art.stages[lives])
