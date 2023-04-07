219edd51da0871cd39c7134d62247e02f2bbc7ec
wakabibrian <wakabibrian95@gmail.com>
2023-04-07 21:19:54 +0300

    Finished Exercise 1

--- Day8/day8.py
+++ Day8/day8.py
@@ -31,3 +31,19 @@
 # Keyword arguments we assign values to variable names when calling the function, we dont mind about the position.
 
 # greet_with_name(location="Kampala", name="Wakabi")
+
+##### Exercise: Paint Area Calculator
+# You are painting a wall. The instructions on the paint can says that 1 can of paint can cover 5 square meters of wall. 
+# Given a random height and width of wall, calculate how many cans of paint you'll need to buy.
+# number of cans = (wall height x wall width) ÷ coverage per can.
+import math
+
+def paint_calc(height, width, cover):
+    numer_of_cans = math.ceil((height * width) / cover)
+    print(f"You'll need {numer_of_cans} cans of paint.")
+
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+
+paint_calc(width=test_w, height=test_h, cover=coverage)
