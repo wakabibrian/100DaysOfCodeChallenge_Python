ba8ca920bef13fe6843fb3d5e06a27f72cfb7009
wakabibrian <wakabibrian95@gmail.com>
2023-04-08 13:55:08 +0300

    Completed Day 9

--- Day9/Day9.md
+++ Day9/Day9.md
@@ -4,3 +4,4 @@ I am learning "Dictionaries" today.
 Today I learn't;
 1. Python Dictionary
 2. Nesting Lists and Dictionaries
+3. Day 9 Project: Secret Auction
--- Day9/day9.py
+++ Day9/day9.py
@@ -88,28 +88,66 @@
 # You are going to write a program that adds to a travel_log.
 # You can see a travel_log which is a List that contains 2 Dictionaries.
 # Write a function that will work with the following line of code on line 21 to add the entry for Russia to the travel_log.
-travel_log = [
-    {
-        "country": "France",
-        "total_visits": 12,
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-    },
-    {
-        "country": "Germany",
-        "total_visits": 5,
-        "cities_visited": ["Berlin", "Hmburg", "Stuttgart"],
-    }
-]
+# travel_log = [
+#     {
+#         "country": "France",
+#         "total_visits": 12,
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#     },
+#     {
+#         "country": "Germany",
+#         "total_visits": 5,
+#         "cities_visited": ["Berlin", "Hmburg", "Stuttgart"],
+#     }
+# ]
 
-def add_new_country(country_visited, no_of_visities, cities):
-    new_country = {}
-    new_country["coutry"] = country_visited
-    new_country["total_visits"] = no_of_visities
-    new_country["cities_visited"] = cities
+# def add_new_country(country_visited, no_of_visities, cities):
+#     new_country = {}
+#     new_country["coutry"] = country_visited
+#     new_country["total_visits"] = no_of_visities
+#     new_country["cities_visited"] = cities
 
-    travel_log.append(new_country)
+#     travel_log.append(new_country)
 
 
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log)
 
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log)
+# -----------------Day 9 Project: Secret Auction-------------------#
+from art import logo
+print("Welcome to the secret auction program.")
+
+print(logo)
+
+bidders = []
+
+end_bid = False
+
+while not end_bid:
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+
+    bidder = {}
+    bidder[name] = bid
+    bidders.append(bidder)
+
+    to_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+
+    if to_continue == "yes":
+        import os
+        def clear(): return os.system("clear")
+        clear()
+    else:
+        end_bid = True
+
+highest_bid = 0
+highest_bidder = ""
+
+for name in bidders:
+
+    for bidder in name:
+        if name[bidder] > highest_bid:
+            highest_bid = name[bidder]
+            highest_bidder += bidder
+
+print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
