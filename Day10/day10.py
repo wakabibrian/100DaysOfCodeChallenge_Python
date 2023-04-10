100babb0f0a8c5a26f92a83f5e7e3accfb261b01
wakabibrian <wakabibrian95@gmail.com>
2023-04-10 08:57:33 +0300

    Finished Docstrings

--- Day10/day10.py
+++ Day10/day10.py
@@ -43,31 +43,51 @@
 # The List month_days contains the number of days in a month from January to December for a non-leap year. 
 # A leap year has 29 days in February.
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-               return False 
-        else:
-            return True
-    else:
-        return False
+# def is_leap(year):
+#     if year % 4 == 0:
+#         if year % 100 == 0:
+#             if year % 400 == 0:
+#                 return True
+#             else:
+#                return False 
+#         else:
+#             return True
+#     else:
+#         return False
 
-def days_in_month(year, month):
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    leap_year = is_leap(year)
+# def days_in_month(year, month):
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     leap_year = is_leap(year)
 
-    if month > 12 or month < 1:
-        return "Invalid month"
-    elif leap_year and month == 2:
-        return 29
-    else:
-        return month_days[month-1]
+#     if month > 12 or month < 1:
+#         return "Invalid month"
+#     elif leap_year and month == 2:
+#         return 29
+#     else:
+#         return month_days[month-1]
 
-#🚨 Do NOT change any of the code below 
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+# #🚨 Do NOT change any of the code below 
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
+
+# -----------------Docstrings-------------------#
+# Docstrings help us to create documentation as we are coding along in our functions or in other blocks of code
+# Forexample it helps to tell what the function is going to do (When you call the function)
+# Docstrings has to go as the first line after the declaration
+# You use 3 quotation marks
+
+def format_name(f_name, l_name):
+    """ Takes a first and last name and fomart it to return the title case version of the name"""
+
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs"
+    
+    first_name = f_name.title()
+    last_name = l_name.title()
+    return f"{first_name} {last_name}"
+
+formated_name = format_name(input("Enter first name: "), input("Enter first name: "))
+print(formated_name)
+
