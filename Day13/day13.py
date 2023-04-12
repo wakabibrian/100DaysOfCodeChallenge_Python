5be43687fccc6516b40bddb035dc65ef0f892bfb
wakabibrian <wakabibrian95@gmail.com>
2023-04-12 22:44:15 +0300

    Finished Describing a problem

---
+++ Day13/day13.py
@@ -0,0 +1,17 @@
+# Debugging - The process of removing bugs from your code
+###########Tips and techniques of finding bugs and removing them
+
+# -----------------Describe a problem-------------------#
+# def my_function():
+# #   for i in range(1, 20):
+#   for i in range(1, 21): # Correct
+#     if i == 20:
+#       print("You got it")
+# my_function()
+# The for loop is supposed to print when i reaches with an assumption of 20, but actually range doesnt reach 20
+
+# -----------------Reproduce the Bug-------------------#
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+print(dice_imgs[dice_num])
