5b5f32bb10c5b5398eef1a08ec0c37514e0349f8
wakabibrian <wakabibrian95@gmail.com>
2023-04-13 06:04:57 +0300

    Finished random choices

--- Day14/day14.py
+++ Day14/day14.py
@@ -1,9 +1,16 @@
-from art import logo
+from art import logo, vs
+import random
+from game_data import data
+
 
 # Print game logo from art.py file
 print(logo)
 
-# Find two ramdom names from game_data file for comparison. Store them in A and B
+# Find two ramdom indices from game_data file for comparison. Store them in A and B
+compare_a = random.choice(data)
+compare_b = random.choice(data)
+print(compare_a["name"])
+print(compare_b["name"])
 
 # Compare function returns answer: Ask user which name has more followers between A and B. 
 # If user chose A and followers A are higher than B return B else return 0
