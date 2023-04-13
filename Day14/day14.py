fb1239096abd00d5b6e407c4d449e0136827514d
wakabibrian <wakabibrian95@gmail.com>
2023-04-13 23:36:01 +0300

    Completed Day 14

--- Day14/day14.py
+++ Day14/day14.py
@@ -3,42 +3,50 @@ import os
 from art import logo, vs
 from game_data import data
 
+print(logo)
+score = 0
+
 
 def clear():
     return os.system("clear")
 
 
 def compare(choice_a, choice_b):
-    print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
+    print(
+        f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
     print(vs)
-    print( f"Compare B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
+    print(
+        f"Against B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
 
-    answer = input(f"Who has more followers? Type 'A' or 'B': ")
+    answer = input(f"Who has more followers? Type 'A' or 'B': ").lower()
 
-    if answer == "A" and choice_a["follower_count"] > choice_b["follower_count"]:
+    if answer == "a" and choice_a["follower_count"] > choice_b["follower_count"]:
         return choice_b
-    elif answer == "B" and choice_b["follower_count"] > choice_a["follower_count"]:
-        return choice_a 
+    elif answer == "b" and choice_b["follower_count"] > choice_a["follower_count"]:
+        return choice_b
     else:
         return 0
 
 
-def play_game():
-
-    print(logo)
-    score = 0
-
-    compare_a = random.choice(data)
+compare_a = random.choice(data)
+compare_b = random.choice(data)
+if compare_a == compare_b:
     compare_b = random.choice(data)
+
+
+compare_next = compare(compare_a, compare_b)
+
+clear()
+print(logo)
+
+while compare_next != 0:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+
+    compare_a = compare_next
+    while compare_a == compare_b:
+        compare_b = random.choice(data)
 
     compare_next = compare(compare_a, compare_b)
 
-    while compare_next != 0:
-        score += 1
-        print(f"You're right! Current score: {score}.")
-
-        compare_next = compare(compare_next, random.choice(data))
-    print(f"Sorry, that's wrong. Final score: {score}")
-
-
-play_game()
+print(f"Sorry, that's wrong. Final score: {score}")
