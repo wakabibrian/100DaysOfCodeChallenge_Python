4aed5d58bc5d2489fdfcc21514935d66203caead
wakabibrian <wakabibrian95@gmail.com>
2023-04-13 05:56:38 +0300

    Finished game details

--- Day14/day14.py
+++ Day14/day14.py
@@ -1,1 +1,14 @@
-# Print logo from art.py file
+from art import logo
+
+# Print game logo from art.py file
+print(logo)
+
+# Find two ramdom names from game_data file for comparison. Store them in A and B
+
+# Compare function returns answer: Ask user which name has more followers between A and B. 
+# If user chose A and followers A are higher than B return B else return 0
+# If user chose B and followers B are higher than A return A right else wrong
+
+# Score function:  If answer not 0, clear screen, increase score by 1.
+# Call compare function with A having answer from previous and B having new random
+# else end game
