63ab40c346bb28fb9d590a5b18c957e2291ea508
wakabibrian <wakabibrian95@gmail.com>
2023-04-13 21:23:51 +0300

    Finished logic

--- Day14/day14.py
+++ Day14/day14.py
@@ -2,10 +2,14 @@ from art import logo, vs
 import random
 from game_data import data
 
-# Compare function returns answer: Ask user which name has more followers between A and B. 
-# If user chose A and followers A are higher than B return B else return 0
-# If user chose B and followers B are higher than A return A right else wrong
+score = 0
+
+
 def compare(choice_a, choice_b):
+    print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
+    print(vs)
+    print(f"Compare B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
+
     answer = input(f"Who has more followers? Type 'A' or 'B': ")
 
     if answer == "A":
@@ -19,20 +23,17 @@ def compare(choice_a, choice_b):
         else:
             return 0
 
-# Score function:  If answer not 0, clear screen, increase score by 1.
-# Call compare function with A having answer from previous and B having new random
-# else end game
-def score()
-        
 
-
-# Print game logo from art.py file
 print(logo)
 
-# Find two ramdom indices from game_data file for comparison. Store them in A and B
 compare_a = random.choice(data)
 compare_b = random.choice(data)
 
-print(f"Compare A: {compare_a['name']}, a {compare_a['description']}, from {compare_a['country']}.")
-print(vs)
-print(f"Compare B: {compare_b['name']}, a {compare_b['description']}, from {compare_b['country']}.")
+compare_next = compare(compare_a, compare_b)
+
+while compare_next != 0:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+    compare_next = compare(compare_next, random.choice(data))
+
+print(f"Sorry, that's wrong. Final score: {score}")
