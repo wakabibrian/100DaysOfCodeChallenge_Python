c353b9f96a91be2fa5be0ef6fb9b05bb769ddc3a
wakabibrian <wakabibrian95@gmail.com>
2023-04-13 22:17:17 +0300

    Finished Logic

--- Day14/day14.py
+++ Day14/day14.py
@@ -1,39 +1,44 @@
-from art import logo, vs
 import random
+import os
+from art import logo, vs
 from game_data import data
 
-score = 0
+
+def clear():
+    return os.system("clear")
 
 
 def compare(choice_a, choice_b):
     print(f"Compare A: {choice_a['name']}, a {choice_a['description']}, from {choice_a['country']}.")
     print(vs)
-    print(f"Compare B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
+    print( f"Compare B: {choice_b['name']}, a {choice_b['description']}, from {choice_b['country']}.")
 
     answer = input(f"Who has more followers? Type 'A' or 'B': ")
 
-    if answer == "A":
-        if choice_a["follower_count"] > choice_b["follower_count"]:
-            return choice_b
-        else:
-            return 0
-    elif answer == "B":
-        if choice_b["follower_count"] > choice_a["follower_count"]:
-            return choice_a
-        else:
-            return 0
+    if answer == "A" and choice_a["follower_count"] > choice_b["follower_count"]:
+        return choice_b
+    elif answer == "B" and choice_b["follower_count"] > choice_a["follower_count"]:
+        return choice_a 
+    else:
+        return 0
 
 
-print(logo)
+def play_game():
 
-compare_a = random.choice(data)
-compare_b = random.choice(data)
+    print(logo)
+    score = 0
 
-compare_next = compare(compare_a, compare_b)
+    compare_a = random.choice(data)
+    compare_b = random.choice(data)
 
-while compare_next != 0:
-    score += 1
-    print(f"You're right! Current score: {score}.")
-    compare_next = compare(compare_next, random.choice(data))
+    compare_next = compare(compare_a, compare_b)
 
-print(f"Sorry, that's wrong. Final score: {score}")
+    while compare_next != 0:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+
+        compare_next = compare(compare_next, random.choice(data))
+    print(f"Sorry, that's wrong. Final score: {score}")
+
+
+play_game()
