aa576a52d0ed52e78a3f22aebb285d240ab87ff3
wakabibrian <wakabibrian95@gmail.com>
2023-04-14 20:20:44 +0300

    Finished implementing off

--- Day15/day15.py
+++ Day15/day15.py
@@ -31,6 +31,7 @@ resources = {
 }
 
 money = 0
+should_continue = True
 
 # 3. Print report.
 ##       a. When the user enters “report” to the prompt, a report should be generated that shows
@@ -73,28 +74,31 @@ def check_resources(user_input, resources):
     elif user_input == "cappuccino":
         return resources["water"] >= cappuccino_water and resources["milk"] >= cappuccino_milk and resources["coffee"] >= cappuccino_coffee
 
-
-# 1. Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
-##       a. Check the user’s input to decide what to do next.
-##       b. The prompt should show every time action has completed, e.g. once the drink is
-##          dispensed. The prompt should show again to serve the next customer.
-user_request = input("  What would you like? (espresso/latte/cappuccino): ").lower()
-
-if user_request == "report":
-    report()
-elif user_request == "espresso" or user_request == "latte" or user_request == "cappuccino":
-    print(check_resources(user_request, resources))
-
-# 2. Turn off the Coffee Machine by entering “ off ” to the prompt.
-##       a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
-##       the machine. Your code should end execution when this happens.
-
 # Process coins.
 ##      a. If there are sufficient resources to make the drink selected, then the program should
 ##          prompt the user to insert coins.
 ##      b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
 ##      c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
 ##          pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+
+while should_continue:
+    # 1. Prompt user by asking “ What would you like? (espresso/latte/cappuccino): ”
+    ##       a. Check the user’s input to decide what to do next.
+    ##       b. The prompt should show every time action has completed, e.g. once the drink is
+    ##          dispensed. The prompt should show again to serve the next customer.
+    user_request = input("  What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if user_request == "report":
+        report()
+    elif user_request == "espresso" or user_request == "latte" or user_request == "cappuccino":
+        print(check_resources(user_request, resources))
+    # 2. Turn off the Coffee Machine by entering “ off ” to the prompt.
+    ##       a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
+    ##       the machine. Your code should end execution when this happens.
+    elif user_request == "off":
+        should_continue = False
+
+
 
 # Check transaction successful?
 ##      a. Check that the user has inserted enough money to purchase the drink they selected.
