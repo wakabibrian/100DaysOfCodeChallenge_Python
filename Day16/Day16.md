442131e38e396e7d28d2d94205d3cf03086add6f
wakabibrian <wakabibrian95@gmail.com>
2023-04-15 21:04:52 +0300

    Finished Turtle example

--- Day16/Day16.md
+++ Day16/Day16.md
@@ -3,4 +3,5 @@ I am learning "OOP" today.
 
 Today I learn't;
 1. Why do we need OOP - Object Oriented Programming?
-2. How to use OOP: Objects andc Classes
+2. How to use OOP: Objects and Classes
+2. How to create Objects and classes
--- Day16/day16.py
+++ Day16/day16.py
@@ -7,7 +7,7 @@
 # Different people can work on different modules simultaneously
 # Each pieces become reusable if we need the same functionality in the future
 
-# -----------------How to use OOP: Objects andc Classes-------------------#
+# -----------------How to use OOP: Objects and Classes-------------------#
 # In OOP, we are trying to model real life objects
 # When modeling objects, we think of what it has and what it does
 # What it has - attributes (variables).
@@ -16,3 +16,25 @@
 #### We can have multiple objects generated from the same type: from the same blue print.
 # In OOP, a blue of an object is called a class
 # Objects are generated from a blue print called class
+
+# -----------------How to create objeects and classes-------------------#
+# Classes are always written with first letter of each word capitalised (Pascal Case), 
+# differentiating it from all variables and function names
+# car = CarBluePrint(), 
+# car - object created from a CarBluePrint() class
+
+from turtle import Turtle, Screen
+
+# creating an object
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+my_screen = Screen()
+
+# object attributes, what the my_screen object has
+print(my_screen.canvheight) # Accessing the attribute
+
+# object methods, what the my_screen object does
+my_screen.exitonclick()
