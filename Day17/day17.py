8f5289180c87f73c3c8758472f42148407339a04
wakabibrian <wakabibrian95@gmail.com>
2023-04-16 19:07:53 +0300

    Created a question bank

--- Day17/day17.py
+++ Day17/day17.py
@@ -57,5 +57,14 @@
 # print(user_1.following)
 
 # -----------------Day 17 Project: Quiz-------------------#
+from data import question_data
+from question_model import Question
 
+question_bank = []
+
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
