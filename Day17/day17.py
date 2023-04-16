80fff4c39c22a5fec55daf28657f8da1b93b2c3a
wakabibrian <wakabibrian95@gmail.com>
2023-04-16 14:06:49 +0300

    finished adding attributes

--- Day17/day17.py
+++ Day17/day17.py
@@ -1,9 +1,43 @@
 # -----------------How to create own classes-------------------#
 # Remember - A class is a blue print for creating an object
 # Syntax
-class User: 
-# The name of the class should have the first name of each letter capitalised (PascalCase)
-# PascalCase - classes, snake_case for other things
-    pass
+# class User: 
+#     pass
+# # The name of the class should have the first name of each letter capitalised (PascalCase)
+# # PascalCase - classes, snake_case for other things
 
-user_1 = User()
+# user_1 = User()
+
+# -----------------Working with Attributes, Class Constructors and the __init__() Functions-------------------#
+# Creating attributes from objects: not a good way
+# An attribute is a variable associated with an object
+# user_1.id = "0001"
+# user_1.name = "Wakabi"
+# print(user_1.name)
+
+# Creating attributes using a connstructor: Better way
+# Constructor specifies what should happen when the object is created: initializing an object 
+# When an object is created, we can set variables to their starting values
+# In python, to create a constructor we use the __init__ function
+# __init__ function is used to initialise the attributes
+# class Car:
+#     def __init__(self):
+#         pass
+#         #initialise attributes
+
+class User:
+    def __init__(self, user_id, username):
+        # Remember attributes are the things that the object will have (variables associated with the final object)
+        # self represents the object that was created from a particular class (its a convention, you can use any word.)
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        
+
+
+# The __init__ fununction will always be called everytime you will create a new object
+user_1 = User("0001", "Wakabi")
+user_2 = User("0002", "Britney")
+print(user_1.username)
+print(user_2.id)
+print(user_1.followers)
