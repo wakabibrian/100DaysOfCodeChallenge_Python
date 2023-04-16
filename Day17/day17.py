62ab10fed93e110e745475fc50cec953d7538f39
wakabibrian <wakabibrian95@gmail.com>
2023-04-16 19:37:03 +0300

    Finished the QuizBrain Method

--- Day17/day17.py
+++ Day17/day17.py
@@ -59,6 +59,7 @@
 # -----------------Day 17 Project: Quiz-------------------#
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -68,3 +69,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+quiz = QuizBrain(question_bank)
+quiz.next_question()
