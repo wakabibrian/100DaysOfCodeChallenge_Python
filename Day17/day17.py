d9c5d4a927234488be0a3dbcc904363321ffe8e7
wakabibrian <wakabibrian95@gmail.com>
2023-04-16 20:55:38 +0300

    Completed Day 17

--- Day17/day17.py
+++ Day17/day17.py
@@ -59,12 +59,14 @@
 from data import question_data
 from question_model import Question
 from quiz_brain import QuizBrain
+import html
 
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    # Use html.unescape() to convert from HTML to Text.
+    question_text = html.unescape(question["question"])
+    question_answer = html.unescape(question["correct_answer"])
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
@@ -72,3 +74,6 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
