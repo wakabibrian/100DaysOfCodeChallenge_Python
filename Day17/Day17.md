9e32e2b885bb9362b26eb841398a4c6b11f4cc0d
wakabibrian <wakabibrian95@gmail.com>
2023-04-16 13:13:06 +0300

    finished creating classes

--- Day17/Day17.md
+++ Day17/Day17.md
@@ -1,4 +1,5 @@
 # Day 17
 Today, I am learning "creating classes" and building "Quiz Game" project.
 
-1. 
+Today I learn't;
+1. How to create own classes
--- Day17/day17.py
+++ Day17/day17.py
@@ -1,1 +1,9 @@
-# -----------------How to Add Python Packages and use PyPi-------------------#
+# -----------------How to create own classes-------------------#
+# Remember - A class is a blue print for creating an object
+# Syntax
+class User: 
+# The name of the class should have the first name of each letter capitalised (PascalCase)
+# PascalCase - classes, snake_case for other things
+    pass
+
+user_1 = User()
