4dbed88b3cf543166f2e8f69e7985e72754cfdb6
wakabibrian <wakabibrian95@gmail.com>
2023-04-18 22:18:54 +0300

    Finished game setup

--- Day19/Day19.md
+++ Day19/Day19.md
@@ -5,3 +5,4 @@ Today I learn't;
 1. Python Higher Order Functions & Event Listeners
 2. Challenge: Make an Etch-A-Sketch App
 3. Object State and Instances
+4. Day 19 Project: Turtle Race
--- Day19/day19.py
+++ Day19/day19.py
@@ -74,3 +74,22 @@
 
 # Their attributes and methods are known as state
 # They have different states
+
+# -----------------Day 19 Project: Turtle Race-------------------#
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make a Bet", prompt="Which Turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -30, 10, 50, 90, 130]
+
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
+
+
+screen.exitonclick()
+
