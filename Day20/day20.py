7159ff0902393c302bfe1775f8ef76b4824db4d4
wakabibrian <wakabibrian95@gmail.com>
2023-04-19 18:53:21 +0300

    Finished Moving snake

--- Day20/day20.py
+++ Day20/day20.py
@@ -26,8 +26,14 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segment in segments:
-        segment.forward(20)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
+
 
 
 screen.exitonclick()
