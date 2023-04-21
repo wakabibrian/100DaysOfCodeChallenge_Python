e6c7b0f8b5e626778e432ee454bef17c9ac034a8
wakabibrian <wakabibrian95@gmail.com>
2023-04-22 02:01:18 +0300

    Completed Day 22

--- Day22/day22.py
+++ Day22/day22.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,18 +13,19 @@ screen.tracer(0)
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
-screen.onkey(right_paddle.go_up, "Up")
-screen.onkey(right_paddle.go_down, "Down")
-screen.onkey(left_paddle.go_up, "w")
-screen.onkey(left_paddle.go_down, "s")
+screen.onkeypress(right_paddle.go_up, "Up")
+screen.onkeypress(right_paddle.go_down, "Down")
+screen.onkeypress(left_paddle.go_up, "w")
+screen.onkeypress(left_paddle.go_down, "s")
 
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,16 +34,21 @@ while game_is_on:
         ball.bounce_y()
     
     # Collision with right_paddle and left_paddle
-    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320:
         ball.bounce_x()
+    
+    if ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_y()
 
     # Detect when right paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.left_point()
     
     # Detect when left paddle misses
     if ball.xcor() < -380:
         ball.reset_position()
+        scoreboard.right_point()
 
 
 screen.exitonclick()
