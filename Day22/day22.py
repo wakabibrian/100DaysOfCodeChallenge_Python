f96293cb3d8685507a92e455e4d8e4e7375a50fb
wakabibrian <wakabibrian95@gmail.com>
2023-04-21 15:45:13 +0300

    Created the second paddle

--- Day22/day22.py
+++ Day22/day22.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.title("Wakabi Pong Game")
@@ -6,27 +7,16 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_len=1, stretch_wid=5)
-paddle.penup()
-paddle.goto(350, 0)
-
-
-def go_up():
-    y_position = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), y_position)
-
-
-def go_down():
-    y_position = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), y_position)
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
 
 game_is_on = True
 while game_is_on:
