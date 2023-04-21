a1e53fe09aad84682da21d0ec63c75566bd174e3
wakabibrian <wakabibrian95@gmail.com>
2023-04-22 00:36:49 +0300

    Finished collision with wall

--- Day22/ball.py
+++ Day22/ball.py
@@ -5,9 +5,14 @@ class Ball(Turtle):
         super().__init__()
         self.shape("circle")
         self.color("white")
+        self.x_move = 10
+        self.y_move = 10
     
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.penup()
         self.goto(x=new_x, y=new_y)
+    
+    def bounce(self):
+        self.y_move *= -1
--- Day22/day22.py
+++ Day22/day22.py
@@ -27,5 +27,9 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    # Detect collision with the wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
 
 screen.exitonclick()
