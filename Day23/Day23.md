6e75932594182b9ecad14b0fcd3cafcef8b9d600
wakabibrian <wakabibrian95@gmail.com>
2023-04-22 12:50:31 +0300

    Finished Detecting Car collision with the player

--- Day23/Day23.md
+++ Day23/Day23.md
@@ -4,3 +4,4 @@ Project Steps;
 
 - Step1: Move the turtle with keypress
 - Step2: Create and Move the cars
+- Step3: Detect Car Collision with the player
--- Day23/day23.py
+++ Day23/day23.py
@@ -21,3 +21,10 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_car()
+
+    # Detect collision with the car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
