d4e58f5cdce5b9a710519aa88617a72cb23c54a3
wakabibrian <wakabibrian95@gmail.com>
2023-04-23 23:27:33 +0300

    Added the txt file

--- Day24/Day24.md
+++ Day24/Day24.md
@@ -2,4 +2,5 @@ Today, I am learning "Files, Directories and Paths"
 
 I learn't;
 
-1.
+1. Add High score to snake game
+2. How to Open, Read, and Write to Files using the "with" Keyword
--- Day24/day24.py
+++ Day24/day24.py
@@ -1,49 +1,54 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
-import time
+# ----------How to Open, Read, and Write to Files using the "with" Keyword---------------#
+file = open("./my_file.txt")
+contents = file.read()
+print(contents)
 
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
-screen.tracer(0)
+# from turtle import Screen
+# from snake import Snake
+# from food import Food
+# from scoreboard import Scoreboard
+# import time
 
-snake = Snake()
-food = Food()
-scoreboard = Scoreboard()
+# screen = Screen()
+# screen.setup(width=600, height=600)
+# screen.bgcolor("black")
+# screen.title("My Snake Game")
+# screen.tracer(0)
 
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+# snake = Snake()
+# food = Food()
+# scoreboard = Scoreboard()
 
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+# screen.listen()
+# screen.onkey(snake.up, "Up")
+# screen.onkey(snake.down, "Down")
+# screen.onkey(snake.left, "Left")
+# screen.onkey(snake.right, "Right")
 
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+#     snake.move()
 
-    # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        # game_is_on = False
-        scoreboard.reset()
-        snake.reset()
+#     # Detect collision with food
+#     if snake.head.distance(food) < 15:
+#         food.refresh()
+#         snake.extend()
+#         scoreboard.increase_score()
 
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            scoreboard.reset()
-            snake.reset()
+#     # Detect collision with wall
+#     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+#         # game_is_on = False
+#         scoreboard.reset()
+#         snake.reset()
+
+#     # Detect collision with tail
+#     for segment in snake.segments[1:]:
+#         if snake.head.distance(segment) < 10:
+#             scoreboard.reset()
+#             snake.reset()
 
 
-# scoreboard.game_over()
-screen.exitonclick()
+# # scoreboard.game_over()
+# screen.exitonclick()
