8fffa23e92c30e37d85a1f7781134276bee3aaef
wakabibrian <wakabibrian95@gmail.com>
2023-04-23 23:52:54 +0300

    Finished  Reading and Writing

--- Day24/day24.py
+++ Day24/day24.py
@@ -1,54 +1,65 @@
 # ----------How to Open, Read, and Write to Files using the "with" Keyword---------------#
-file = open("./my_file.txt")
-contents = file.read()
-print(contents)
+# Reading the file
+# with open("./my_file.txt") as file:  # with keyword helps in opening and closing the file
+#     contents = file.read()
+#     print(contents)
 
-# from turtle import Screen
-# from snake import Snake
-# from food import Food
-# from scoreboard import Scoreboard
-# import time
+# Writing to the existing file
+# with open("my_file.txt", mode="a") as file:
+#     # By default mode - r (read only)
+#     # w - write (erases and writes new text)
+#     # a - append (adds text to the existing)
+#     file.write("\nMy text.")
 
-# screen = Screen()
-# screen.setup(width=600, height=600)
-# screen.bgcolor("black")
-# screen.title("My Snake Game")
-# screen.tracer(0)
+# Writing to the non existing file - creates the file, mode has to always be w
+# with open("new_file.txt", mode="w") as file:
+#     file.write("\nMy text.")
 
-# snake = Snake()
-# food = Food()
-# scoreboard = Scoreboard()
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
 
-# screen.listen()
-# screen.onkey(snake.up, "Up")
-# screen.onkey(snake.down, "Down")
-# screen.onkey(snake.left, "Left")
-# screen.onkey(snake.right, "Right")
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-# game_is_on = True
-# while game_is_on:
-#     screen.update()
-#     time.sleep(0.1)
-#     snake.move()
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-#     # Detect collision with food
-#     if snake.head.distance(food) < 15:
-#         food.refresh()
-#         snake.extend()
-#         scoreboard.increase_score()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-#     # Detect collision with wall
-#     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-#         # game_is_on = False
-#         scoreboard.reset()
-#         snake.reset()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
-#     # Detect collision with tail
-#     for segment in snake.segments[1:]:
-#         if snake.head.distance(segment) < 10:
-#             scoreboard.reset()
-#             snake.reset()
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        # game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
-# # scoreboard.game_over()
-# screen.exitonclick()
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+# scoreboard.game_over()
+screen.exitonclick()
