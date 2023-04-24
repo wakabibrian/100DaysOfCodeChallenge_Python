daf797554bdb457da206f326c6c5ee20d533e049
wakabibrian <wakabibrian95@gmail.com>
2023-04-24 23:49:36 +0300

    Finished Day 24

--- Day24/day24.py
+++ Day24/day24.py
@@ -95,14 +95,21 @@
 
 # ----------Day 24 Project: Mail Merge Project---------------#
 # TODO: Create a letter using starting_letter.txt
-with open("Input/Letters/starting_letter.txt") as letter:
-    contents = letter.readlines()
-    print(contents)
-
 # for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
+# Replace [name] with name
 # Save the letters in the folder "ReadyToSend".
 
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+PLACEHOLDER = "[name]"
+
+with open("Input/Names/invited_names.txt") as names_file:
+    names_list = names_file.readlines()
+
+with open("Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+
+    for name in names_list:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+
+        with open(f"Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as new_letter_file:
+            new_letter_file.write(new_letter)
