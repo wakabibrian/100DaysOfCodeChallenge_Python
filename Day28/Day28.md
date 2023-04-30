bb33b6046efed45b43f628f8cad2ac16afd1d7c7
wakabibrian <wakabibrian95@gmail.com>
2023-05-01 00:14:31 +0300

    Finished the challenge

--- Day28/Day28.md
+++ Day28/Day28.md
@@ -5,3 +5,4 @@ Today, I am learning "Tkinter, Dynamic Typing and Pomodoro project"
 I learn't:
 
 1. Day28 Project: Pomodoro - How to work with the Canvas Widget and Add Images to Tkinter
+2. Day28 Project: Challenge - Complete the Application's User Interface (UI)
--- Day28/day28.py
+++ Day28/day28.py
@@ -29,7 +29,20 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white",
                    font=(FONT_NAME, 28, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
+
+title_lable = Label(text="Timer", bg=YELLOW, fg=GREEN,
+                    font=(FONT_NAME, 30, "bold"))
+title_lable.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
