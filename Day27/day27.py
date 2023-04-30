8c089b32b7536ced3d0491c694a24c2903a8612c
wakabibrian <wakabibrian95@gmail.com>
2023-04-30 05:26:18 +0300

    Finished creating windows and labels with TKinter

--- Day27/day27.py
+++ Day27/day27.py
@@ -1,3 +1,19 @@
 # ---------------------History of GUI and Introduction to Tkinter-------------------#
 # Tkinter inter can be used in creating Graphical User Interfaces
 # GUI are different from using consoles
+
+# ---------------------Creating Windows and Labels with Tkinter-------------------#
+import tkinter
+
+# Creating a window
+window = tkinter.Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+
+# Creating a Label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 18, "bold"))
+my_label.pack(expand=True)
+
+
+# Keeping the window running
+window.mainloop()
