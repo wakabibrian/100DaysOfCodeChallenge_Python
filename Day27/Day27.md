2f0fb5901c386f067eb202b219eaca54e20ecc3d
wakabibrian <wakabibrian95@gmail.com>
2023-04-30 05:39:08 +0300

    Finished creating functions with default values

--- Day27/Day27.md
+++ Day27/Day27.md
@@ -6,3 +6,4 @@ I learn't:
 
 1. History of GUI and Introduction to Tkinter
 2. Creating Windows and Labels with Tkinter
+3. Setting Default Values for Optional Arguments inside a Function Header
--- Day27/day27.py
+++ Day27/day27.py
@@ -3,17 +3,28 @@
 # GUI are different from using consoles
 
 # ---------------------Creating Windows and Labels with Tkinter-------------------#
-import tkinter
+# import tkinter
 
-# Creating a window
-window = tkinter.Tk()
-window.title("My first GUI program")
-window.minsize(width=500, height=300)
+# # Creating a window
+# window = tkinter.Tk()
+# window.title("My first GUI program")
+# window.minsize(width=500, height=300)
 
-# Creating a Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 18, "bold"))
-my_label.pack(expand=True)
+# # Creating a Label
+# my_label = tkinter.Label(text="I am a label", font=("Arial", 18, "bold"))
+# my_label.pack(expand=True)
 
 
-# Keeping the window running
-window.mainloop()
+# # Keeping the window running
+# window.mainloop()
+
+# ---------------------Setting Default Values for Optional Arguments inside a Function Header-------------------#
+# Using Advanced Python Arguments inorder to specify a wider range of inputs
+# Arguments with default values
+def my_function(a=1, b=2, c=3):
+    pass
+
+
+my_function()  # No need to provide the inputs in the function calls
+# changing one of the default value, the rest take on the default values
+my_function(b=5)
