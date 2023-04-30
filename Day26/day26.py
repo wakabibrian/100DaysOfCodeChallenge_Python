3bda1023e8e1f53dc3e46ca2c3fa173893e17e1e
wakabibrian <wakabibrian95@gmail.com>
2023-04-30 04:35:01 +0300

    Completed Day 26

--- Day26/day26.py
+++ Day26/day26.py
@@ -133,3 +133,17 @@
 #         print(row.score)
 
 # ------------------Day 26 Project: NATO Alphabet---------------------#
+import pandas
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+word_dict = {row.letter: row.code for (
+    index, row) in nato_data_frame.iterrows()}
+# print(word_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter a word: ").upper()
+result = [word_dict[letter] for letter in user_input]
+print(result)
