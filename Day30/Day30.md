95220f728b93774924240a915ea1ebda065fc925
wakabibrian <wakabibrian95@gmail.com>
2023-05-02 20:00:54 +0300

    Completed raising own errors

--- Day30/Day30.md
+++ Day30/Day30.md
@@ -5,3 +5,4 @@ Today, I am learning "Errors, Exceptions and JSON Data"
 I tearnt;
 
 1. Catching Exceptions: The try catch except finally Pattern
+2. Raising your own Exceptions
--- Day30/day30.py
+++ Day30/day30.py
@@ -46,3 +46,33 @@
 # finally:  # Not often used
 #     file.close()
 #     print("File was closed")
+
+# ------------- Raising your own Exceptions-------------#
+# You can raise your own exceptions incase your code encounters an expected situations
+# raise keyword: helps us to raise our own exceptions
+# try:
+#     file = open("a_file.txt")
+#     dict = {"key": "value"}
+#     print(dict["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error I made up")
+
+# --- When to raise errors?
+# # Perfect code but going to raise the wrong result
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))
+
+# if height > 3:
+#     # ValueError: whatever value that was entered as argument was wrong
+#     raise ValueError("Human height should not be over 3 meters")
+
+# bmi = weight / height**2
+# print(bmi)
