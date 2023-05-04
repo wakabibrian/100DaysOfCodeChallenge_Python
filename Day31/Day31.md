68a208a9749e0c550d91e1a22a471927b327664d
wakabibrian <wakabibrian95@gmail.com>
2023-05-05 02:27:38 +0300

    Finished next card

---
+++ Day31/Day31.md
@@ -0,0 +1,10 @@
+## Day 31
+
+Today, I am Building a "Flash Card App Capstone Project"
+Find project code [here](Day31/day31.py)
+
+Steps for the project;
+
+1. Create the User Interface (UI) with Tkinter
+2. Create New Flash Cards
+3. Flip cars
--- Day31/day31.py
+++ Day31/day31.py
@@ -1,6 +1,21 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+# -------------------Create New Flash Cards-------------------#
+
+
+def next_card():
+    current_card = random.choice(to_learn)
+
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -12,17 +27,23 @@ canvas = Canvas(width=800, height=526,
                 highlightthickness=0, bg=BACKGROUND_COLOR)
 front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=front_img)
-canvas.create_text(400, 156, text="French", font=("Arial", 30, "italic"))
-canvas.create_text(400, 263, text="trouve", font=("Arial", 50, "bold"))
+card_title = canvas.create_text(
+    400, 156, text="title", font=("Arial", 30, "italic"))
+card_word = canvas.create_text(
+    400, 263, text="trouve", font=("Arial", 50, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 # buttons
 right_img = PhotoImage(file="images/right.png")
 wrong_img = PhotoImage(file="images/wrong.png")
 
-right_button = Button(image=right_img, highlightthickness=0)
+right_button = Button(image=right_img, highlightthickness=0,
+                      borderwidth=0, command=next_card)
 right_button.grid(column=1, row=1)
-wrong_button = Button(image=wrong_img, highlightthickness=0)
+wrong_button = Button(image=wrong_img, highlightthickness=0,
+                      borderwidth=0, command=next_card)
 wrong_button.grid(column=0, row=1)
+
+next_card()
 
 window.mainloop()
