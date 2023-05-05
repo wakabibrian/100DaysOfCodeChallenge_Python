d661843b7c8e77fdd4d6bf2f61e5861cd996e10c
wakabibrian <wakabibrian95@gmail.com>
2023-05-06 02:16:54 +0300

    Finished Smtplib Module

--- Day32/Day32.md
+++ Day32/Day32.md
@@ -5,3 +5,4 @@ Today, I am learning "Send Email(smtplib) and Manage Dates(datetime)"
 I have learn't;
 
 1. How to Send Emails with Python using SMTP
+2. Working with the datetime Module
--- Day32/day32.py
+++ Day32/day32.py
@@ -5,3 +5,22 @@
 # The recipient computer gets from the recipient email server (e.g Yahoo Email Server)
 
 # Python smtplib allows us to send emails to any address on the internet
+
+# import smtplib
+
+# my_email = "wakabibrian23@gmail.com"
+# password = "zdbjynhcskeypjfi"
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:  # Connecting to the email server
+#     # TLS - Transport Layers Security; A way of securing our connection to our email server
+#     # Encrypts the email for other people not to read it
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="wakabibrian23@yahoo.com",
+#         msg="Subject:Hello\n\nThis is the body of my email"
+#     )
+#     connection.close()
+
+# ------------Working with the datetime Module--------------#
