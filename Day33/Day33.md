034a7b544cf18bef8a81890d575af93a0d6018ac
wakabibrian <wakabibrian95@gmail.com>
2023-05-08 02:57:46 +0300

    Finished endpoints

--- Day33/Day33.md
+++ Day33/Day33.md
@@ -5,3 +5,4 @@ Today, I am learning "API Endpoints and API Parameters"
 I learn't;
 
 1. What are Application Programming Interfaces (APIs)?
+2. API Endpoints and Making API Calls
--- Day33/day33.py
+++ Day33/day33.py
@@ -3,3 +3,16 @@
 # that programmers can use to create software or interact with an external system
 # Through API, you use your program to make a Request to an external system for some piece of data
 # Then the external system will Respond to you appropriately and give the datas
+
+# ----------------API Endpoints and Making API Calls--------------------#
+# An API Endpoint is one of the most important aspect of an API
+# You can imagine it as a location; location the data is stored
+# An API Endpoint is simply a URL (location where the data you want can be found)
+# You also have to make a request over the internet (API request) - requesting data from the endpoint
+# API request - simply requesting/getting a piece of data using their API
+import requests
+
+# url takes the endpoint
+# returns a response code
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+print(response)
