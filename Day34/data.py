a8777231a0273adbc6ee1b452f9ca239e036809c
wakabibrian <wakabibrian95@gmail.com>
2023-05-08 23:28:02 +0300

    Completed challenge 1

--- Day34/data.py
+++ Day34/data.py
@@ -2,13 +2,14 @@ import requests
 
 parameters = {
     "amount": 10,
-    "type": boolean
+    "type": "boolean"
 }
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
 
 data = response.json()
+question_data = data["results"]
 
 # question_data = [
 #     {
