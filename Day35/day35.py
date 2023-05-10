51b60f4e27d69fba5d103b3cf7b0a2d80aaebbbe
wakabibrian <wakabibrian95@gmail.com>
2023-05-10 23:01:22 +0300

    Finished checking rain

--- Day35/day35.py
+++ Day35/day35.py
@@ -7,16 +7,20 @@
 # hence authorise or denie access
 
 # --- Using API Keys to Authenticate and Get the Weather from OpenWeatherMap ---#
+# --- Challenge - Check if it Will Rain in the Nextx 12 Hours ---#
+
 import requests
 
 api_key = "69f04e4613056b159c2761a9d9e664d2"
-MY_LAT = 0.3747545
-MY_LONG = 32.5572151
+# MY_LAT = 0.3747545
+# MY_LONG = 32.5572151
+MY_LAT = 23.731939
+MY_LONG = -99.150818
 
 parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
-    "exclude": "current, minutely, daily, alerts",
+    "exclude": "current,minutely,daily",
     "appid": api_key
 }
 
@@ -24,5 +28,20 @@ response = requests.get(
     "https://api.openweathermap.org/data/2.5/onecall", params=parameters)
 response.raise_for_status()
 
-data = response.json()
-print(data)
+weather_data = response.json()
+
+weather_slice = weather_data["hourly"][:12]
+condition_codes = [hour_data["weather"][0]["id"]
+                   for hour_data in weather_slice]
+
+if any(condition_codes) < 700:
+    print("Bring an Umbrella")
+
+# will_rain = False
+
+# for condition_code in condition_codes:
+#     if condition_code < 700:
+#         will_rain = True
+
+# if will_rain:
+#     print("Bring an Umbrella")
